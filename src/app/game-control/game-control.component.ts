9afa396750ff3fb213bc0a27a49c2cb61477effe
mcarolinadiaz <diazmariacarolina6@gmail.com>
2023-04-04 17:00:16 -0300

    second task complete and add two buttons into gameControl component

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class GameControlComponent {
 
+  onStartGame() {
+    console.log('start!');
+  }
+
+  onClear() {
+    console.log('stop!');
+  }
 }
