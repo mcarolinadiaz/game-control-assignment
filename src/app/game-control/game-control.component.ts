6b99b55fbaee426078c3b40bb70c12c819c321d7
mcarolinadiaz <diazmariacarolina6@gmail.com>
2023-04-04 17:17:23 -0300

    add setInterval and clearInterval. so, holding a incrementing number

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent {
+  number: number = 0;
+  intervalId: any;
 
   onStartGame() {
     console.log('start!');
+
+    this.intervalId = setInterval( () => {this.number += 1}, 3600);
   }
 
   onClear() {
     console.log('stop!');
+    this.number = 0;
+    clearInterval(this.intervalId);
   }
 }
